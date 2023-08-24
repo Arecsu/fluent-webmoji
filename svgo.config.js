b5d45b5e8a6199bd0f246ac2047efa1bc780676a
Arecsu <alejandro9r@gmail.com>
2023-08-24 15:18:10 -0300

    fix IDs wrong colors when many SVGs are inlined in the HTML

--- svgo.config.js
+++ svgo.config.js
@@ -8,6 +8,7 @@ module.exports = {
           // viewBox is required to resize SVGs with CSS.
           // @see https://github.com/svg/svgo/issues/1128
           removeViewBox: false,
+          cleanupIds: false,
         },
       },
     },
